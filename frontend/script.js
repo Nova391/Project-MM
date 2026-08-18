5dc24b4fa22bf9f0c738d6901dabb849ff84faab
mohamed <mohamedabukhalil5@gmail.com>
2026-08-18 15:54:40 +0300

    Improve frontend and add account modal

--- frontend/script.js
+++ frontend/script.js
@@ -1,9 +1,25 @@
 const menuButton = document.querySelector("#menuButton");
 const sidebar = document.querySelector(".sidebar");
+const maincontent = document.querySelector("#maincontent");
+const addaccount = document.querySelector("#addaccount");
+const accountForm = document.querySelector(".account-form")
+const modal = document.querySelector(".modal");
+const closeModal = document.querySelector("#closeModal");
 
 console.log(menuButton);
 console.log(sidebar);
 
 menuButton.addEventListener("click", function () {
     sidebar.classList.toggle("hidden")
+    maincontent.classList.toggle("shifted");
+})
+
+addaccount.addEventListener("click", function () {
+    accountForm.classList.toggle("hidden");
+    modal.classList.toggle("hidden");
+})
+
+closeModal.addEventListener("click", function () {
+    accountForm.classList.toggle("hidden");
+    modal.classList.toggle("hidden");
 })
